c27c2b745602ea745b1679d4d419d7afdbf35155
Fela afria ningsih <113352248+Felaafrianingsih1@users.noreply.github.com>
2022-10-20 21:58:17 +0800

    Add files via upload

---
+++ POSTTEST5/input.php
@@ -0,0 +1,12 @@
+<?php 
+include 'Koneksi.php';
+$tujuan = $_POST['tujuan'];
+$harga = $_POST['harga'];
+$paket = $_POST['paket'];
+
+
+mysqli_query($connection,"INSERT INTO booking VALUES('','$tujuan','$harga','$paket')");     
+   
+header("location:dashboard.php");
+
+?>
